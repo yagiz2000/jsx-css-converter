eff829869b312f04dd52e0d66425d60525e1a4e5
Yağız <yagzceritoglu@gmail.com>
2023-10-16 13:36:04 +0300

    refactor: remove redundant code

--- src/extension.ts
+++ src/extension.ts
@@ -13,27 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
 
-  let disposable = vscode.commands.registerCommand(
-    "jsx-css-converter.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-
-      const cssCode: string = convertInlineStyleToCSS(`{
-        display: "-webkit-box",
-        maxHeight: "44px",
-        fontSize: "14px",
-        lineHeight: "19px",
-        textOverflow: "ellipsis",
-        overflow: "hidden",
-        WebkitLineClamp: 2,
-        WebkitBoxOrient: "vertical",
-      }`);
-
-      vscode.window.showInformationMessage(`Converted CSS: ${cssCode}`);
-    }
-  );
-  const disposable2 = vscode.commands.registerCommand(
+  const disposable = vscode.commands.registerCommand(
     "jsx-css-converter.getSelectedValues",
     () => {
       const editor = vscode.window.activeTextEditor;
@@ -46,7 +26,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(disposable);
-  context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
