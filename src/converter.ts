de83796904d60b17e5bf941cb863beefcef75099
Yağız <yagzceritoglu@gmail.com>
2023-10-16 13:01:26 +0300

    feat: by style tag

--- src/converter.ts
+++ src/converter.ts
@@ -1,6 +1,10 @@
 import * as json5 from "json5";
 
 function convertInlineStyleToCSS(selection: string): string {
+  selection = selection.trim();
+  if (selection.includes("style=")) {
+    selection = selection.split("style=")[1].slice(1, -1);
+  }
   const styleObject = json5.parse(selection);
 
   const css = Object.keys(styleObject)
