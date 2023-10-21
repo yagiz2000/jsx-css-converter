e728aeefe5af90679bcefcac0748a45eb7b47a47
Yağız <yagzceritoglu@gmail.com>
2023-10-21 16:02:51 +0300

    feat: test case for styles wrapped by html tags

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,21 +1,7 @@
 import * as assert from "assert";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
 import { convertInlineStyleToCSS } from "../../converter";
 
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
-
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
-});
-//can you write tests for the converter.ts file?
-suite("Convert with style tag", () => {
+suite("Multiple Conversion Test Suites ", () => {
   test("Convert style tag to css ends with double parentheses", () => {
     const css = convertInlineStyleToCSS(`style={{color: "red"}}`);
     assert.strictEqual(css, "color: red");
@@ -50,6 +36,18 @@ suite("Convert with style tag", () => {
   test("Convert without curly parentheses and ends with comma", () => {
     const css = convertInlineStyleToCSS(
       `color: 'red',display:'flex',justifyContent:'center',fontFamily:'Arial',`
+    );
+    assert.equal(
+      css,
+      "color: red; display: flex; justify-content: center; font-family: 'Arial'"
+    );
+  });
+
+  test("Convert style tag wrapped by HTML tags", () => {
+    const css = convertInlineStyleToCSS(
+      `<div
+      style={{color: "red",display:"flex",justifyContent:"center",fontFamily:'Arial',}}
+    ></div>`
     );
     assert.equal(
       css,
