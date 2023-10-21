561ee39b7c447013338ee75b55c9af76b3cf7adc
Yağız <yagzceritoglu@gmail.com>
2023-10-21 14:30:18 +0300

    feat: without curly paranthesess conversion

--- src/converter.ts
+++ src/converter.ts
@@ -5,6 +5,10 @@ function convertInlineStyleToCSS(selection: string): string {
   if (selection.includes("style=")) {
     selection = selection.split("style=")[1].slice(1, -1);
   }
+  if (!selection.includes("{")) {
+    selection = selection.replaceAll(`\n`, "");
+    selection = `{${selection}}`;
+  }
   const styleObject = json5.parse(selection);
 
   const css = Object.keys(styleObject)
@@ -13,7 +17,13 @@ function convertInlineStyleToCSS(selection: string): string {
         /[A-Z]/g,
         (match) => `-${match.toLowerCase()}`
       );
-      return `${cssProperty}: ${styleObject[property]}`;
+      let value;
+      if (property === "fontFamily") {
+        value = `'${styleObject[property]}'`;
+      } else {
+        value = styleObject[property];
+      }
+      return `${cssProperty}: ${value}`;
     })
     .join("; ");
 
